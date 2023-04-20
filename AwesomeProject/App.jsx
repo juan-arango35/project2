bb6a92585222381b3e55f93ccfedbe4a4a183911
juan <juanarangozapata35@gmail.com>
2023-04-20 12:53:07 -0500

    Agregando algunos estilos sencillos

--- AwesomeProject/App.jsx
+++ AwesomeProject/App.jsx
@@ -1,10 +1,33 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function App() {
+
+  const style = StyleSheet.create({
+  
+  })
+
   return (
-    <View>
-      <Text> cambiamos a la rama main</Text>
+    <View style={{flexDirection: "row-reverse"}} >
+  
+    <View style={{flex:2, backgroundColor: "red", justifyContent: "center", alignItems:"center", justifyContent:"center"}} >  
+      <Text style={{color: "white"}}>
+        Juan joel arango
+
+      </Text>
+      
     </View>
+
+    <View style={{flex: 1, backgroundColor: "gray" , justifyContent:"center", alignItems:"center"}} >  
+      <Text style={{color:"green"}} >
+        Juan joel arango
+
+      </Text>
+      
+    </View>
+    </View>
+    
   )
 }
+
+
